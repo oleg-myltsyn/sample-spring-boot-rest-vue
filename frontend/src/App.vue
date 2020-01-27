8b4adcc2f3325d0facb51dc0e714c968065e4fde
Олег <oleg5432101@rambler.ru>
2020-01-27 13:48:55 +0300

    added client table component (vue-tables-2)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Table msg="Welcome to Your App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Table from "./components/Table";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Table
+    // HelloWorld
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import {ClientTable} from "vue-tables-2";
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import {BootstrapVue} from 'bootstrap-vue';
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(ClientTable, {options:{}, useVuex:false, theme:'bootstrap4', template:'default'});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
